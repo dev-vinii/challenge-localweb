33d606a5cf3ab146454764569921bec27f0bc76a
Vinicius <viniciusluna@yahoo.com>
2024-09-26 01:02:38 -0300

    refact: app name on router file

--- src/routes/email.ts
+++ src/routes/email.ts
@@ -8,20 +8,20 @@ import {
 
 import { FastifyInstance } from "fastify";
 
-export default async function emailRoutes(fastify: FastifyInstance) {
-  fastify.get("/email", async (request, reply) => {
+export default async function emailRoutes(app: FastifyInstance) {
+  app.get("/email", async (request, reply) => {
     return getEmails(request, reply);
   });
-  fastify.get("/email/spam", async (request, reply) => {
+  app.get("/email/spam", async (request, reply) => {
     return getSpam(request, reply);
   });
-  fastify.post("/email", async (request, reply) => {
+  app.post("/email", async (request, reply) => {
     return createEmail(request, reply);
   });
-  fastify.put("/email/:id", async (request, reply) => {
+  app.put("/email/:id", async (request, reply) => {
     return updateEmail(request, reply);
   });
-  fastify.delete("/email/:id", async (request, reply) => {
+  app.delete("/email/:id", async (request, reply) => {
     return deleteEmail(request, reply);
   });
 }
--- src/routes/event.ts
+++ src/routes/event.ts
@@ -6,17 +6,17 @@ import {
   updateEvent,
 } from "../controllers/event";
 
-export default async function eventRoutes(fastify: FastifyInstance) {
-  fastify.get("/event", async (request, reply) => {
+export default async function eventRoutes(app: FastifyInstance) {
+  app.get("/event", async (request, reply) => {
     return getEvents(request, reply);
   });
-  fastify.post("/event", async (request, reply) => {
+  app.post("/event", async (request, reply) => {
     return createEvent(request, reply);
   });
-  fastify.put("/event/:id", async (request, reply) => {
+  app.put("/event/:id", async (request, reply) => {
     return updateEvent(request, reply);
   });
-  fastify.delete("/event/:id", async (request, reply) => {
+  app.delete("/event/:id", async (request, reply) => {
     return deleteEvent(request, reply);
   });
 }
